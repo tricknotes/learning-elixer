656909cf324018af11c9f991ec7b89a8ff219f4b
Ryunosuke Sato <tricknotes.rs@gmail.com>
2016-09-10 13:16:19 +0900

    Align indent

--- basic-types/with-match.exs
+++ basic-types/with-match.exs
@@ -1,9 +1,9 @@
 result = with {:ok, file}   = File.open("/etc/passwd"),
-          content       = IO.read(file, :all),
-          :ok           = File.close(file),
-          [_, uid, gid] <- Regex.run(~r{_xxx:.*?:(\d+):(\d+)}, content)
-    do
-          "Group: #{gid}, User: #{uid}"
-    end
+              content       = IO.read(file, :all),
+              :ok           = File.close(file),
+              [_, uid, gid] <- Regex.run(~r{_xxx:.*?:(\d+):(\d+)}, content)
+         do
+              "Group: #{gid}, User: #{uid}"
+         end
 
 IO.puts result
--- basic-types/with-scope.exs
+++ basic-types/with-scope.exs
@@ -4,9 +4,9 @@ lp = with {:ok, file}   = File.open("/etc/passwd"),
           content       = IO.read(file, :all),
           :ok           = File.close(file),
           [_, uid, gid] = Regex.run(~r{_lp:.*?:(\d+):(\d+)}, content)
-    do
+     do
           "Group: #{gid}, User: #{uid}"
-    end
+     end
 
 IO.puts lp
 IO.puts content
