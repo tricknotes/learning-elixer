2e14f619d837a3868cdb72aba6f4b04836e88b4e
Ryunosuke Sato <tricknotes.rs@gmail.com>
2016-09-10 21:08:01 +0900

    Add example 3 for function
    p.39

--- function/example-2.exs
+++ function/example-2.exs
@@ -1,11 +1,25 @@
-fizzbuzz = fn
+# example-2
+puts_fizzbuzz = fn
   0, 0, _     -> "FizzBuzz"
   0, _, _     -> "Fizz"
   _, 0, _     -> "Buzz"
   _, _, value -> value
 end
 
-"FizzBuzz" = fizzbuzz.(0, 0, 0)
-"Fizz"     = fizzbuzz.(0, 1, 0)
-"Buzz"     = fizzbuzz.(1, 0, 0)
-"No match" = fizzbuzz.(1, 1, "No match")
+"FizzBuzz" = puts_fizzbuzz.(0, 0, 0)
+"Fizz"     = puts_fizzbuzz.(0, 1, 0)
+"Buzz"     = puts_fizzbuzz.(1, 0, 0)
+"No match" = puts_fizzbuzz.(2, 1, "No match")
+
+# example-3
+fizzbuzz = fn
+  n -> puts_fizzbuzz.(rem(n, 3), rem(n, 5), n)
+end
+
+"Buzz"    = fizzbuzz.(10)
+11        = fizzbuzz.(11)
+"Fizz"    = fizzbuzz.(12)
+13        = fizzbuzz.(13)
+14        = fizzbuzz.(14)
+"FizzBuzz"= fizzbuzz.(15)
+16        = fizzbuzz.(16)
