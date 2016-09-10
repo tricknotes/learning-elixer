1370553650e9d44d67bef2cbe8d0ff2f70063f8c
Ryunosuke Sato <tricknotes.rs@gmail.com>
2016-09-10 20:08:58 +0900

    Add example for function
    p.37

---
+++ function/example-1.exs
@@ -0,0 +1,17 @@
+list_concat = fn
+  list1, list2 -> list1 ++ list2
+end
+
+[:a, :b, :c, :d] = list_concat.([:a, :b], [:c, :d])
+
+sum = fn
+  a, b, c -> a + b + c
+end
+
+6 = sum.(1, 2, 3)
+
+pair_tuple_to_list = fn
+  {a, b} -> [a, b]
+end
+
+[1234, 5678] = pair_tuple_to_list.({1234, 5678})
