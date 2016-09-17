de6959eee49db9ed1829580c9cbcc5ff7fd6b05d
Ryunosuke Sato <tricknotes.rs@gmail.com>
2016-09-17 23:05:31 +0900

    Add `import` example
    p.57

---
+++ mm/import.exs
@@ -0,0 +1,13 @@
+defmodule Example do
+  def func1 do
+    List.flatten [1, [2, 3], 4]
+  end
+
+  def func2 do
+    import List, only: [flatten: 1]
+    flatten [1, [2, 3], 4]
+  end
+end
+
+IO.inspect Example.func1
+IO.inspect Example.func1
