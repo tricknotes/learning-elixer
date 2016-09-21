aa1b68a8061b585aca8e084745fddd8e5e99e228
Ryunosuke Sato <tricknotes.rs@gmail.com>
2016-09-22 02:43:14 +0900

    Add `MyList.max/1`
    p.71

--- lists/example.exs
+++ lists/example.exs
@@ -1,4 +1,9 @@
 defmodule MyList do
   def mapsum([], _func),           do: 0
   def mapsum([head | tail], func), do: func.(head) + mapsum(tail, func)
+
+  def max(list), do: _max(list, 0)
+
+  defp _max([], current),            do: current
+  defp _max([head | tail], current), do: if current > head, do: _max(tail, current), else: _max(tail, head)
 end
