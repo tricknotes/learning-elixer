2e6f8cb8f00df59c784f3acd9dfed79c9a225fc3
Ryunosuke Sato <tricknotes.rs@gmail.com>
2016-09-24 01:18:43 +0900

    Update nested list match example
    p.73

--- lists/wether.exs
+++ lists/wether.exs
@@ -1,9 +1,9 @@
 defmodule WetherHistory do
-  def for_location_27([]), do: []
-  def for_location_27([[time, 27, temp, rain] | tail]) do
-    [[time, 27, temp, rain] | for_location_27(tail)]
+  def for_location([], _target_loc), do: []
+  def for_location([[time, target_loc, temp, rain] | tail], target_loc) do
+    [[time, target_loc, temp, rain] | for_location(tail, target_loc)]
   end
-  def for_location_27([_ | tail]), do: for_location_27(tail)
+  def for_location([_ | tail], target_loc), do: for_location(tail, target_loc)
 
   def test_data do
     [
